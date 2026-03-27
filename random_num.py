d3dfdfe197ad23c85bf84fed154365c2d208e30a
Brandon Avendano <163790950+Alley-Wars@users.noreply.github.com>
2026-03-27 10:42:42 -0400

    Update random_num.py

--- random_num.py
+++ random_num.py
@@ -1,24 +1,24 @@
 import random
 
-random_num = random.randint(1, 11)
-count = 0
-attempt = 4
-while count < 3:
-    count += 1
-    for n in range(random_num):
-        try:
-            if attempt == 1:
-                print("Well times up!")
-                break
-            
-            attempt = max(0, attempt - 1)
-            guess_num = int(input(f"Hello pick a number you only have {attempt} chances! "))
-            
-            if guess_num == random_num:
-                print("You got it!")
-                break
-            else:
-                print("Next time buddy")
-    
-        except ValueError:
-            print("Error numbers only!")
+random_num = random.randint(1, 10)
+attempt = 3
+
+while attempt > 0:
+    try:
+        guess_num = int(input(f"Hello pick a number you only have {attempt} chances! "))
+        
+        if guess_num == random_num:
+            print("You got it!")
+            break
+        else:
+            print("Next time buddy")
+            attempt -= 1
+
+    except ValueError:
+        print("Error numbers only!")
+
+if attempt > 0:
+    print("YOU WON!!")
+
+if attempt == 0:
+    print(f"GAME OVER! The number was {random_num}")
